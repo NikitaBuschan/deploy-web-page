6b31d1f06c3b1cbe71197f98b0a0706939d32a79
Nikita <nikitabushhan@gmail.com>
2022-06-10 21:17:47 +0300

    add project

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtRKgX8Esx282V9HBl-lIauuRn7dnOC8w",
+  authDomain: "deploy-b82d6.firebaseapp.com",
+  projectId: "deploy-b82d6",
+  storageBucket: "deploy-b82d6.appspot.com",
+  messagingSenderId: "449116336329",
+  appId: "1:449116336329:web:2f3062b0c41b99bcc75d18"
+};
+
+const app = initializeApp(firebaseConfig);
